f97dd7cac79ed11458d56231b3e22d829dfa1a33
David Krause <enthus1ast@users.noreply.github.com>
2022-01-24 10:00:04 +0100

    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

---
+++ readme.md
@@ -0,0 +1,51 @@
+a seq unpacker and mini `orm`
+
+```nim
+type Obj = object
+  bb: int
+  cc: bool
+var se = ["1337","somethingToSkip", "foo", "1", "true"]
+var id: int
+var obj = Obj()
+se.to(id, nil, obj)
+assert obj.aa == "foo"
+assert obj.bb == 1
+assert obj.cc == true
+```
+
+
+```nim
+import db_sqlite
+
+type
+  Foo = object
+    first: string
+    second: string
+    third: float
+    forth: int
+  Foo2 = object
+    first: string
+    second: string
+    third: float
+    forth: int
+
+var se = @["foo", "baa", "13.37", "123"]
+
+var db = open(":memory:", "", "", "")
+
+# Easy to create tables based on types.
+db.exec(sql ct(Foo)) # create tables based on the types
+db.exec(sql ct(Foo2)) # create tables based on the types
+
+# Easy to insert, based on types.
+for idx in 0..10:
+  db.exec(sql ci(Foo), $idx & "first", "second", "13.37", "123")
+  db.exec(sql ci(Foo2), $idx & "fasdfwefew", "fwef32fwef3", "13.37", "123")
+
+# Just write any sql, then unpack the types.
+for row in db.getAllRows(sql"select * from Foo, Foo2 where Foo.id = Foo2.id"):
+  var foo: Foo = Foo()
+  var foo2: Foo2 = Foo2()
+  to(row, nil, foo, nil, foo2) # skip elements with nil (eg: table id's)
+  # ... use foo and foo2
+```
--- src/nisane.nim
+++ src/nisane.nim
@@ -1,7 +1,8 @@
 # TODO
 # DONE bool
 # DONE ref object
-# TODO to macro "_" should skip
+# DONE to macro "nil" should skip
+# TODO defaults and modifiers for the create macros with custom pragmas
 # Beef: Depending on what you want you could use disruptek's assume package which has typeit allowing you to iterate fields of objects
 # Beef: https://github.com/disruptek/assume/blob/master/tests/typeit.nim#L102-L199
 
@@ -22,22 +23,21 @@ macro to*(se: untyped, tys: varargs[typed]): typed =
   ## a generic seq/openarray unpacker.
   runnableExamples:
     type Obj = object
-      aa: string
       bb: int
       cc: bool
-    var se = ["foo", "1", "true"]
+    var se = ["1337","somethingToSkip", "foo", "1", "true"]
+    var id: int
     var obj = Obj()
-    se.to(obj)
+    se.to(id, nil, obj)
     assert obj.aa == "foo"
     assert obj.bb == 1
     assert obj.cc == true
-
-  echo "to se:", repr se
   result = newStmtList()
-
   var seqidx = 0
   for ty in tys:
-
+    if (repr ty) == "nil": # Skip this type
+      seqidx.inc
+      continue
     # echo "####"
     # echo treeRepr(ty.getTypeImpl())
     # echo "end####"
@@ -45,36 +45,28 @@ macro to*(se: untyped, tys: varargs[typed]): typed =
     let (kind, tyy) = ty.gType
     case kind
     of TyString:
-      # echo "BT: string"
       let ex = $ty & " = "  & (repr se)  & "[" & $seqidx  & "]"  & ".toString"
       result.add parseStmt(ex)
       seqidx.inc
     of TyInt:
-      # echo "BT: int"
       let ex = $ty & " = "  & (repr se)  & "[" & $seqidx  & "]"  & ".toInt"
       result.add parseStmt(ex)
       seqidx.inc
     of TyBool:
-      # echo "BT: int"
       let ex = $ty & " = "  & (repr se)  & "[" & $seqidx  & "]"  & ".toBool"
       result.add parseStmt(ex)
       seqidx.inc
     of TyFloat:
-      # echo "BT: float"
       let ex = $ty & " = "  & (repr se)  & "[" & $seqidx  & "]"  & ".toFloat"
       result.add parseStmt(ex)
       seqidx.inc
     of TyObj:
-      # echo "OBJECT TYPE"
-      # echo treeRepr(ty.getType())
       for idx, el in ty.getTypeImpl[2].pairs:
-        echo idx, " ",  repr el
         let ex = $ty & "." & toStrLit(el[0]).strval & " = "  & (repr se)  & "[" & $seqidx  & "]" & ".to" & el[1].strval.capitalizeAscii
         result.add parseStmt(ex)
         seqidx.inc
     of TyRefObj:
       for idx, el in tyy[2][2].pairs:
-        echo idx, " ",  repr el
         let ex = $ty & "." & toStrLit(el[0]).strval & " = "  & (repr se)  & "[" & $seqidx  & "]" & ".to" & el[1].strval.capitalizeAscii
         result.add parseStmt(ex)
         seqidx.inc
@@ -88,9 +80,7 @@ macro to*(se: untyped, tys: varargs[typed]): typed =
       # echo repr ty.getTypeImpl().getTypeImpl().getTypeImpl() #getImplTransformed()
       echo "end####"
     of TyTuple:
-      # echo "tuple"
       for idx, el in ty.getTypeImpl.pairs:
-        # echo "TU", idx, " ",  repr el
         let ex = $ty & "." & toStrLit(el[0]).strval & " = "  & (repr se)  & "[" & $seqidx  & "]" & ".to" & el[1].strval.capitalizeAscii
         result.add parseStmt(ex)
         seqidx.inc
@@ -123,8 +113,6 @@ macro ct*(ty: typed, mapping: proc(nimType: string): string = mapToSqlType): str
   case kind
   of TyObj:
     for idx, el in tyy[2].pairs:
-      echo treeRepr el
-      echo idx, " ",  repr el
       let name = toStrLit(el[0]).strval
       let nimType = el[1].strval
       let sqlType = nimType.mapToSqlType()
@@ -227,14 +215,9 @@ when isMainModule and true:
     db.exec(sql ci(Foo2), $idx & "fasdfwefew", "fwef32fwef3", "13.37", "123")
 
   for row in db.getAllRows(sql"select * from Foo, Foo2 where Foo.id = Foo2.id"):
-    echo row
-    var idFoo: int = -1
     var foo: Foo = Foo()
-
-    expandMacros:
-      to(row, idFoo, foo)
-    echo idFoo
-    # print idFoo, foo, idBaa, baa
+    var foo2: Foo2 = Foo2()
+    to(row, nil, foo, nil, foo2) # skip elements with nil (eg: table id's)
 
   var idxA: int = -1
   var idxB: int = -1
@@ -355,6 +338,15 @@ when isMainModule and true:
       var robj = Robj()
       se.to(robj)
       check robj.ii == 1
+      check robj.ss == "foo"
+      check robj.bb == true
+    test "skip with 'nil'":
+      var se = ["1", "foo", "true"]
+      type Obj = object
+        ss: string
+        bb: bool
+      var robj = Obj()
+      se.to(nil, robj)
       check robj.ss == "foo"
       check robj.bb == true
     # test "ref str":
